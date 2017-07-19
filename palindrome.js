78971b1fe89a45561617b759f2e1eb249761913d
fillopfy <abhi456tm@gmail.com>
2017-07-19 10:57:16 +0530

    pallindrome check for a string

---
+++ palindrome.js
@@ -0,0 +1,21 @@
+function palindrome(str){
+
+	var re=/[^A-Za-z0-9]/gi;  //non-alphanumeric characters-regularExpression
+	//replacing all non-alphanumeric chars from str and make it lowercase	
+	var mStr=str.replace(re,"").toLowerCase();
+
+	//split,reverse and join to check for pallindrome
+	var nStr=mStr.split("").reverse().join("");
+	
+	if(mStr===nStr)
+	{
+		return true;
+	}
+
+	else
+	return false;
+
+}
+
+//In short:
+// return str.replace(re,"").toLowerCase()====str.replace(re,"").toLowerCase().split("").reverse("").join//	  ("")?true:false;
